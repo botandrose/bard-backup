8ea0f6976eb77104a86960344cea78df5470f2f5
Micah Geisel <micah@botandrose.com>
2024-07-23 18:48:35 +0200

    stop rolling our own s3 client.

--- lib/bard/backup.rb
+++ lib/bard/backup.rb
@@ -1,10 +1,5 @@
-# frozen_string_literal: true
-
-require "net/http"
-require "openssl"
-require "base64"
-require "time"
 require "backhoe"
+require "bard/backup/s3_dir"
 
 module Bard
   class Backup < Struct.new(:s3_path, :filename, :access_key, :secret_key)
@@ -13,74 +8,10 @@ module Bard
     end
 
     def call
+      path = "/tmp/#{filename}"
       Backhoe.dump path
-
-      self.full_s3_path = "/#{s3_path}/#{filename}"
-
-      if s3_path.include?("/")
-        s3_bucket = s3_path.split("/").first
-        self.s3_path = s3_path.split("/")[1..].join("/")
-        uri = URI("https://#{s3_bucket}.s3.amazonaws.com/#{s3_path}/#{filename}")
-      else
-        uri = URI("https://#{s3_path}.s3.amazonaws.com/#{filename}")
-      end
-
-      request = Net::HTTP::Put.new(uri, {
-        "Content-Length": File.size(path).to_s,
-        "Content-Type": content_type,
-        "Date": date,
-        "Authorization": "AWS #{access_key}:#{signature}",
-        "x-amz-storage-class": "STANDARD",
-        "x-amz-acl": "private",
-      })
-      request.body_stream = File.open(path)
-
-      Net::HTTP.start(uri.hostname) do |http|
-        response = http.request(request)
-        response.value # raises if not success
-      end
-    end
-
-    private
-
-    attr_accessor :full_s3_path
-
-    def signature
-      digester = OpenSSL::Digest::SHA1.new
-      digest = OpenSSL::HMAC.digest(digester, secret_key, key)
-      Base64.strict_encode64(digest)
-    end
-
-    def key
-      [
-        "PUT",
-        "",
-        content_type,
-        date,
-        acl,
-        storage_type,
-        full_s3_path,
-      ].join("\n")
-    end
-
-    def content_type
-      "application/gzip"
-    end
-
-    def date
-      Time.now.rfc2822
-    end
-
-    def acl
-      "x-amz-acl:private"
-    end
-
-    def storage_type
-      "x-amz-storage-class:STANDARD"
-    end
-
-    def path
-      "/tmp/#{filename}"
+      s3_dir = S3Dir.new(path: s3_path, access_key:, secret_key:)
+      s3_dir.put path
     end
   end
 end
--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,6 +1,6 @@
+require "bard/backup"
+require "bard/backup/s3_dir"
 require "json"
-require "aws-sdk-s3"
-require "rexml"
 require "debug"
 
 RSpec.describe "Bard::Backup" do
@@ -9,6 +9,7 @@ RSpec.describe "Bard::Backup" do
 
   before do
     s3_dir.empty!
+    # s3_dir.put "tmp/2020-04-19T00:00:00Z.sql.gz", body: "DATA"
     stub_const "Backhoe", FakeBackhoe.new
   end
 
@@ -17,48 +18,6 @@ RSpec.describe "Bard::Backup" do
     expect(s3_dir.keys).to eq([
       "test/2020-04-20T12:30:00Z.sql.gz",
     ])
-  end
-end
-
-class S3Dir < Data.define(:path, :access_key, :secret_key)
-  def keys
-    response = client.list_objects_v2({
-      bucket: bucket_name,
-      prefix: folder_prefix,
-    })
-    raise if response.is_truncated
-    response.contents.map(&:key)
-  end
-
-  def empty!
-    keys.each_slice(1000) do |key_batch|
-      objects_to_delete = key_batch.map { |key| { key: key } }
-      client.delete_objects({
-        bucket: bucket_name,
-        delete: {
-          objects: objects_to_delete,
-          quiet: true,
-        }
-      })
-    end
-  end
-
-  def bucket_name
-    path.split("/").first
-  end
-
-  def folder_prefix
-    path.split("/")[1..].join("/")
-  end
-
-  private
-
-  def client
-    Aws::S3::Client.new({
-      region: "us-west-2",
-      access_key_id: access_key,
-      secret_access_key: secret_key,
-    })
   end
 end
 
